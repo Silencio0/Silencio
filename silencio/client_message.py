560c5b623b3d407d9209588b793309c04ef58a9d
oghodwhy <moena94@hotmail.com>
2017-07-18 11:50:19 -0700

    Lotsa changes to impliment chatrooms, plus some cleanup.
    Implimented in server:
    network.breadcast()
    network.create_chatroom()
    network.destroy_chatroom()
    active_chatroom.__init__()
    active_chatroom.add_user()
    active_chatroom.remove_user()
    
    Some changes to:
    network.send_msg() - changed input type
    active_user - cleanup of constructors
    client_message - cleanup of constructors

--- silencio/client_message.py
+++ silencio/client_message.py
@@ -3,17 +3,14 @@ import datetime
 class message(object):
     """This class is designed as a container for a clientside message and all details needed for simple handling"""
     
-    is_incoming
-    user
-    content
-    timestamp
+
     
     def __init__(self, username, message_string, time_sent):
-        user = username
-        content = message_string
-        timestamp = time_sent
+        self.user = username
+        self.content = message_string
+        self.timestamp = time_sent
 
     def __init__(self, username, message_string):
-        user = username
-        content = message_string
-        timestamp = datetime.time
+        self.user = username
+        self.content = message_string
+        self.timestamp = datetime.now()
