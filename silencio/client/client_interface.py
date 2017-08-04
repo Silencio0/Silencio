94acb9c7fe66046757522f26daba29fa2a773ca0
oghodwhy <moena94@hotmail.com>
2017-08-04 15:13:48 -0700

    Client overhaul
    It actually connects with user settings and does stuff now.

--- silencio/client/client_interface.py
+++ silencio/client/client_interface.py
@@ -3,26 +3,45 @@ import sys
 import select
 class interface(object):
     """Container for the basic text interface and its functions. Basically, just reads the messages to the command line"""
-    
-    def user_login(self):
-    	"""Prompts user for username and password"""
-    	while True:
-    		entered_username =  input('Enter username or /register: ')
-    		if entered_username == "/register":
-    			entered_username = input('Enter desired username: ')
-    			entered_password = input ('Enter desired password: ')
-    			if network.register(entered_username, entered_password) == True:
-    				print("Successfully registered with username: " + entered_username + "\n")
-    				return
-    			else:
-    				print("Unsuccessful registration - try another username\n")
-	    	else:
-	    		entered_password = input('Enter password: ')
-	    		if network.login(entered_username, entered_password) == True:
-	    			print("Successful login\n")
-	    			return
-	    		print("Incorrect username/password\n")
  
+    def __init__(self):
+        my_net = False
+        
+
+    def user_connect(self):
+        """Prompts user for connection address and port, then attempts network connection."""
+        while True:
+            entered_address =  input('Enter server address:\n ')
+            entered_port = input('Enter server port:\n ')
+            self.my_net = network(entered_address, entered_port)
+            if mynet is not False:
+                print("Successfully connected.\n")
+                return True
+
+            else:
+                print("Unable to connect to specified address, please try again.\n")
+                
+    def user_login(self):
+        """Prompts user for username and password"""
+        while True:
+            entered_username =  input('Enter username or /register: ')
+            if entered_username == "/register":
+                entered_username = input('Enter desired username: ')
+                entered_password = input ('Enter desired password: ')
+                if network.register(entered_username, entered_password) == True:
+                    print("Successfully registered with username: " + entered_username + "\n")
+                    return True
+                else:
+                    print("Unsuccessful registration - try another username\n")
+            else:
+                entered_password = input('Enter password: ')
+                if network.login(entered_username, entered_password) == True:
+                    print("Successful login\n")
+                    return True
+
+                else:
+                    print("Incorrect username/password, please try again.\n")
+                    
     def user_listen(self):
     	#listens for user input and sends message if there's input
     	while sys.stdin in select.select([sys.stdin], [], [], 0)[0]:
--- silencio/silencio_client.py
+++ silencio/silencio_client.py
@@ -13,33 +13,18 @@ Seng 299 Summer 2017
 """
 
 from client.client_interface import interface
-from client.client_network import network
-
 
 
 if __name__ == "__main__":
-    server_ip = 'localhost'
-    serverport = 7600
-    username = 'wat'
-    password = 'pass'
-
-
-
-
-    net = network(server_ip, server_port)
+    
     interf = interface()
 
-    if not net:
-        print('Failed to connect to server\n')
+    interf.user_connect()
     
-
     interf.user_login()
 
     while True:
 
         interf.user_listen()    
 
-        out = net.listen()
-        if out is not True and out is not False and out is not []:
-            print(out)
     
