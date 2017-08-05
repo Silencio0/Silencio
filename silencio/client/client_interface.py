e1432d36c2101c42c1a7e2cc4e5e8b28b978c007
oghodwhy <moena94@hotmail.com>
2017-08-04 20:25:34 -0700

    fixed to loop on none, not false.

--- silencio/client/client_interface.py
+++ silencio/client/client_interface.py
@@ -14,7 +14,7 @@ class interface(object):
             entered_address =  input('Enter server address:\n ')
             entered_port = input('Enter server port:\n ')
             self.my_net = network(entered_address, entered_port, None)
-            if self.my_net is not False:
+            if self.my_net is not None:
                 print("Successfully connected.\n")
                 return True
 
